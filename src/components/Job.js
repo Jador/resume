82e259d0593b95f5987dc2734dbab205598e626c
Tyler Graham <tyler.graham.prog@gmail.com>
2016-05-30 15:58:24 -0500

    let's get some content out there

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { section, h2, div } from 'react-hyperscript-helpers';
+import { section, h2, div, p } from 'react-hyperscript-helpers';
 
-export default ({ position, start, end }) =>
+export default ({ position, start, end, location, copy = [] }) =>
   section([
     h2(position),
-    div(`${start} – ${end}`)
+    div(`${start} – ${end}`),
+    div(location),
+    div(copy.map(para => p(para)))
   ])
