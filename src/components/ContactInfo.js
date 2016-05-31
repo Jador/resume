fd66592504c8a83b097fde1836a584cf8bd23828
Tyler Graham <tyler.graham.prog@gmail.com>
2016-05-30 20:26:35 -0500

    cleaning up the contact info a bit

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -3,18 +3,23 @@ import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
 
+import { Card, CardTitle, CardText } from 'material-ui/Card';
+
 import { div, h } from 'react-hyperscript-helpers';
 
 export default ({ contacts }) =>
-  div(contacts.map(contact => {
-    switch (contact.type) {
-      case 'phone': return Phone(contact);
-      case 'email': return Email(contact);
-      case 'medium': return Link(contact, `https://medium.com/@${contact.value}`);
-      default: return Link(contact, `https://${contact.type}.com/${contact.value}`);
-    }
-  }));
-
+  h(Card, { style: { marginTop: '1.5em' } }, [
+    h(CardTitle, { title: 'Drop a Line' }),
+    div({ style: { paddingBottom: '1em' } },
+      contacts.map(contact => {
+        switch (contact.type) {
+          case 'phone': return Phone(contact);
+          case 'email': return Email(contact);
+          case 'medium': return Link(contact, `https://medium.com/@${contact.value}`);
+          default: return Link(contact, `https://${contact.type}.com/${contact.value}`);
+        }
+      }))
+  ]);
 
 const Phone = (contact) => Link(contact, `tel:${contact.value}`);
 
@@ -22,7 +27,9 @@ const Email = (contact) => Link(contact, `mailto:${contact.value}`, 'envelope');
 
 const Link = ({ type, value }, href, icon) =>
   h(FlatButton, {
+    href,
     label: value,
-    linkButton: true, href,
+    linkButton: true,
+    primary: true,
     icon: h(FontIcon, { className: `fa fa-${icon || type}` })
   });
